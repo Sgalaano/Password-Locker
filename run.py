90a8b0dc961ef4f84b961b5a84ecce7a36521800
sgalaano <sgalaano@gmail.com>
2019-05-13 10:42:08 +0300

    added added imported passes

--- run.py
+++ run.py
@@ -1,4 +1,58 @@
+import getpass
+import random
+import string
+from user import User
+from credentials import Credentials
 
+def create_user(username,password1):
+    '''
+    Function to create new user
+    '''
+    new_user = User(username,password1)
+    return new_user
+
+def create_credentials(account_name,password):
+    '''
+    Function to create new account
+    '''
+    new_credentials = User(account_name,password)
+    return new_credentials 
+
+def save_user(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+ 
+def save_credentials(self):
+    '''
+    save_user method to save new users into the list
+    '''
+    Credentials.credentials_list.append(self)
+
+def check_existing_user(password):
+    '''
+    function to check that enable login authentification
+    '''
+    return User.user_exist(password)
+
+def find_account(password):
+    '''
+    function to find account by its name
+    '''
+    return User.find_account(password)
+
+def delete_credentials(self):
+    '''
+    delete method to delete saved information from the list
+    '''
+    Credentials.credentials_list.remove(self)
+
+def display_credentials():
+    '''
+    method that returns the user list
+    '''
+    return User.display_users()
 def Galaano():
   print("WELCOME TO PASSWORD LOCKER.")
 def Galaano():  
@@ -12,7 +66,7 @@ def Galaano():
       print("Press 1 = sign up / Press 2 = log in / Press 3 = exit")
       logorsign = input()
       if logorsign == "1":  
-        print("Awesome!Please enter your preffered username.")
+        print("GREAT!!Please enter your preffered username.")
         username = input()
         print("Enter a preffered password.")
         password1 = getpass.getpass("password:")
